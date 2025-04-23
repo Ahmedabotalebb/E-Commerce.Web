8b578e5ba747afa5c678e8ee79241d68adfefc03
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-04-23 23:42:22 +0200

    04 & 05 Product Module and Dbcontext

---
+++ Persistence/Data/Configuration/ProductCof.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data.Configuration
+{
+    class productConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(P => P.poductBrand)
+                .WithMany()
+                .HasForeignKey(P => P.BrandId);
+
+            builder.HasOne(P => P.poductBrand)
+                            .WithMany()
+                            .HasForeignKey(P => P.BrandId);
+            builder.Property(P => P.Price).HasColumnType("decimal(10,2)");
+        }
+    }
+}
---
+++ Persistence/StoreDbcontext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class StoreDbcontext : DbContext
+    {
+        public StoreDbcontext(DbContextOptions<StoreDbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductBrand> productBrands { get; set; }
+        public DbSet<ProductType> productTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemplyRefernce).Assembly);
+        }
+    }
+}
