306e3bc110f0ee324094eaef265d2766e8083e3f
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-04-23 22:07:52 +0200

    02 API types

---
+++ E-Commerce.Web/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Product : ControllerBase
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
