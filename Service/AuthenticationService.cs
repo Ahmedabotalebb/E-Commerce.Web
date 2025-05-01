a8f7bbcb9644a1bc85c9590130982c54dfd28d48
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-05-01 19:44:22 +0300

    07 JWT & Token Creation

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -1,18 +1,22 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using DomainLayer.Exceptions;
 using DomainLayer.Models.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using ServiceApstraction;
 using Shared.DataTransfereObjects.IdentityDto;
 
 namespace Service
 {
-    class AuthenticationService (UserManager<ApplicationUser> _userManager): IAuthenticationService
+    public class AuthenticationService (UserManager<ApplicationUser> _userManager,IConfiguration configuration): IAuthenticationService
     {
         public async Task<UserDto> LoginAsync(LoginDto loginDto)
         {
@@ -26,7 +30,7 @@ namespace Service
                 {
                     DisplayName = user.DisplayName,
                     Email = user.Email,
-                    Token = CreateTokenAsync(user)
+                    Token = await CreateTokenAsync(user)
                 };
 
             }
@@ -38,10 +42,6 @@ namespace Service
         }
 
 
-        private static string CreateTokenAsync(ApplicationUser user)
-        {
-            return "Token - TODO";
-        }
         public async Task<UserDto> RegisterAsync(RegisterDto registerDto)
         {
             var user = new ApplicationUser()
@@ -54,7 +54,7 @@ namespace Service
             var result = await _userManager.CreateAsync(user, registerDto.Password);
             if (result.Succeeded)
             {
-                return new UserDto() { DisplayName = user.DisplayName, Email = user.Email, Token = CreateTokenAsync(user) };
+                return new UserDto() { DisplayName = user.DisplayName, Email = user.Email, Token =await CreateTokenAsync(user) };
             }
             else
 
@@ -66,6 +66,32 @@ namespace Service
 
             
         }
+        private async Task<string> CreateTokenAsync(ApplicationUser user)
+        {
+            var Claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
 
+            };
+            var Roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in Roles)
+            {
+                Claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var SecretKey = configuration.GetSection("JWTOptions")["SecretKey"];
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var Creds = new SigningCredentials(Key,SecurityAlgorithms.HmacSha256);
+            var Token = new JwtSecurityToken(
+                issuer: configuration.GetSection("JWTOptions")["Issuer"],
+                audience: configuration.GetSection("JWTOptions")["Audience"],
+                claims: Claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: Creds);
+            return new JwtSecurityTokenHandler().WriteToken(Token);
+
+        }
     }
 }
--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -7,14 +7,15 @@ using AutoMapper;
 using DomainLayer.Contracts;
 using DomainLayer.Models.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using ServiceApstraction;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork , IMapper mapper ,IBasketRepository basketRepository , UserManager<ApplicationUser> userManager) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork , IMapper mapper ,IBasketRepository basketRepository , UserManager<ApplicationUser> userManager,IConfiguration configuration) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>((new ProductService(_unitOfWork, mapper)));
-        private readonly Lazy<IAuthenticationService> _authenticationservice = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager));
+        private readonly Lazy<IAuthenticationService> _authenticationservice = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager, configuration));
         public IProductService productService => _LazyProductService.Value;
         private readonly Lazy<IBasketService>  _basketService = new Lazy<IBasketService>(() =>new BasketService(basketRepository,mapper));
         public IBasketService BasketService => _basketService.Value;
