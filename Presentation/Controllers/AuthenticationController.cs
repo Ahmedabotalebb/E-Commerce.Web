3cfe548bc7ef952495c2edbb62c8dce1e6ee3493
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-05-01 17:52:00 +0300

    06 Authentication Controller

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceApstraction;
+using Shared.DataTransfereObjects.IdentityDto;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController(IServiceManager serviceManager):ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user= await serviceManager.authenticationService.LoginAsync(loginDto);
+            return Ok(user);
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user= await serviceManager.authenticationService.RegisterAsync(registerDto);
+            return Ok(user);
+        }
+    }
+}
